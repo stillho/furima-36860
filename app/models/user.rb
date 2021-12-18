9fd98b59c52e386e11f3a18ccefd21500ac87ec5
stillho <yangdehuadao@gmail.com>
2021-12-19 05:13:32 +0900

    バリテーション、rspecの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname,           presence: true
-  validates :email,              presence: true
-  validates :encrypted_password, presence: true
-  validates :last_name,          presence: true
-  validates :first_name,         presence: true
-  validates :read_name_last,     presence: true
-  validates :read_name_first,    presence: true
+  validates :encrypted_password,:password_confirmation, format:{ with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
+    validates :read_name_last
+    validates :read_name_first
+  end
+  
   validates :birthday,           presence: true
 end
 
