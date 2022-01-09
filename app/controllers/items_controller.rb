0c80a046b08238a84c4ce16112c111f03c014c05
stillho <yangdehuadao@gmail.com>
2022-01-10 03:50:13 +0900

    商品情報テーブル保存コミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_price, :category_id, :item_status_id, :cost_id, :prefecture_id, :item_info,
+                                 :aim_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,30 +1,32 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :cost
+  belongs_to :prefecture
+  belongs_to :aim
+
   belongs_to :user
   has_one_attached :image
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category 
-  belongs_to :item_status
-  belongs_to :cost                  
-  belongs_to :prefecture                  
-  belongs_to :aim 
 
   with_options presence: true do
     validates :image
     validates :item_name
-    validates :item_price 
-    validates :category_id 
+    validates :item_price
+    validates :category_id
     validates :item_status_id
     validates :cost_id
-    validates :prefecture_id 
+    validates :prefecture_id
     validates :item_info
-    validates :aim_id 
+    validates :aim_id
   end
 
-  validates :item_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
-  validates :item_price, numericality: { only_integer: true, message: "Half-width number." }
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :item_status_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :cost_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :aim_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_price,
+            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+  validates :item_price, numericality: { only_integer: true, message: 'Half-width number.' }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :cost_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :aim_id, numericality: { other_than: 1, message: "can't be blank" }
 end
