d37c8c8d3429970572c573416682cd3e4e14a437
stillho <yangdehuadao@gmail.com>
2022-01-10 13:45:50 +0900

    金額入力時に販売手数料や販売利益の計算結果が表示　コミット

---
+++ app/javascript/item_price.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+  const market_price = document.getElementById("item-price");
+  market_price.addEventListener("input", () => {
+  const inputValue = market_price.value;
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+  const profitNumber = document.getElementById("profit")
+  const value_result = inputValue * 0.1
+  profitNumber.innerHTML = (Math.floor(inputValue - value_result));
+  console.log(profitNumber);
+  })
+});
