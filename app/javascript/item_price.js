30e5314d494585dea31277a1d456b979286d9ab7
stillho <yangdehuadao@gmail.com>
2022-01-10 14:02:45 +0900

    commit2

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -7,6 +7,5 @@ window.addEventListener('load', () => {
   const profitNumber = document.getElementById("profit")
   const value_result = inputValue * 0.1
   profitNumber.innerHTML = (Math.floor(inputValue - value_result));
-  console.log(profitNumber);
   })
 });
