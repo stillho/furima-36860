88ab606ed475e45aff28571b740673bf1c6a93fa
stillho <yangdehuadao@gmail.com>
2022-01-10 18:47:02 +0900

    修正コミット2

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -9,5 +9,8 @@ FactoryBot.define do
     prefecture_id { 2 }
     aim_id { 2 }
     association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
