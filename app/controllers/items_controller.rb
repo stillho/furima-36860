9fda7a7b16bf653d135facc89075efba29fd81ee
stillho <yangdehuadao@gmail.com>
2022-01-16 15:09:17 +0900

    修正コミット3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.destroy
+    if @item.destroy
       redirect_to root_path
     else
       render :show
