98d1f6a166dc2cd3fa4d47af27c53d7f3b8b9ff6
stillho <yangdehuadao@gmail.com>
2022-03-14 04:25:13 +0900

    コミット2

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,14 +1,13 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :postcode, :prefecture_id, :city, :house_number, :building, :telephone, :item_id, :user_id, :token
-
   with_options presence: true do
     validates :item_id
     validates :user_id
-    validates :postcode, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :postcode, format: { with: /\A\d{3}-\d{4}\z/, message: 'は無効です。 ハイフンを含んでください' }
     validates :city
     validates :house_number
-    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'を選んでください' }
     validates :telephone, format: { with: /\A\d{10,11}\z/ }
     validates :token
   end
